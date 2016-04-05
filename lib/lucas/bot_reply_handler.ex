fbd0a286bf224ba20f7b85b7de002477ab914e97
Vladimir Barsukov <pro.barsukov@gmail.com>
2016-04-05 22:09:29 +0200

    group command support

--- lib/lucas/bot_reply_handler.ex
+++ lib/lucas/bot_reply_handler.ex
@@ -1,9 +1,13 @@
 defmodule Lucas.BotReplyHandler do
-  def reply(%{"message" => %{"text" => "/cat", "chat" => %{"id" => id}}}) do
-    Lucas.Bot.exec_cmd("sendMessage", %{chat_id: id, text: "Meow"})
-  end
 
-  def reply(_) do
-    IO.puts "Not matched"
+  def reply(%{"message" => %{"text" => text, "chat" => %{"id" => id}}}) do
+    cond do
+     text =~ ~r[/cat] ->
+        Lucas.Bot.exec_cmd("sendMessage", %{chat_id: id, text: "Meow!"})
+     text =~ ~r[/gelezka] ->
+       Lucas.Bot.exec_cmd("sendMessage", %{chat_id: id, text: "Никита три мохито!"})
+     true ->
+       IO.puts "Not matched"
+    end
   end
 end
