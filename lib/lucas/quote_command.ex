fe289a4837fd0f90bb3b07133119e7ccf00c5dc6
Vladimir Barsukov <pro.barsukov@gmail.com>
2016-05-21 21:31:58 +0200

    improved inline quotes bot

--- lib/lucas/quote_command.ex
+++ lib/lucas/quote_command.ex
@@ -1,18 +1,26 @@
 defmodule Lucas.QuoteCommand do
 
   @quotes [ "Педроройсы", "Милнер тащун", "Сруболы", "Не маховик", "Надо уважать",
-    "Никита три мохито", "Клайн-иисус", "Боруссия-мехельбах", "Сакочище" ]
+    "Никита три мохито", "Клайн-иисус", "Надо брать", "Боруссия-менхельбах", "Сакочище" ]
 
   def get_random_quote() do
     Enum.random(@quotes)
   end
 
-  def math_by_string(query) do
-    Enum.filter(@quotes, fn(podcastQuote) -> String.starts_with?(podcastQuote, query) end)
+  def match_by_string(query) do
+    downcasedQuery = String.downcase(query)
+    Enum.filter(@quotes, fn(podcastQuote) ->
+      String.starts_with?(String.downcase(podcastQuote), downcasedQuery)
+    end)
   end
 
   def get_quote_by_query(query) do
-    get_quotes = math_by_string(query)
+    length = String.length(query)
+    get_quotes = cond do
+      length > 0 -> match_by_string(query)
+      length == 0 -> @quotes
+    end
+
     encoded_data = Enum.map get_quotes, fn (podcastQuote) ->
       %{
         type: "article",
