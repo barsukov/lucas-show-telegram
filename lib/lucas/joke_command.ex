3579657bf5669c4e61f56ea03c170cbafae5fb31
Vladimir Barsukov <pro.barsukov@gmail.com>
2016-05-21 23:46:45 +0200

    random joke comamnd support

--- lib/lucas/joke_command.ex
+++ lib/lucas/joke_command.ex
@@ -1,10 +1,12 @@
 defmodule Lucas.JokeCommand do
   def get_joke_rank(text) do
-    parts = String.split(text, "/joke ")
-    joke_point = Integer.parse Enum.at(parts, 1)
-
+    parts = String.split(text, "/joke")
+    joke_point = String.strip(Enum.at(parts, 1)) |> Integer.parse
+    if joke_point == :error do
+      random = Enum.to_list 0..10
+      joke_point = {Enum.random(random)}
+    end
     joke_text = cond do
-      joke_point == :error -> "Не нервируй меня малыш"
       elem(joke_point, 0) in 0..4 -> "#{elem(joke_point, 0)} Хондамира из 10"
       elem(joke_point, 0) in 5..10 -> "#{elem(joke_point, 0)} Хондамиров из 10"
       elem(joke_point, 0) > 10 -> "Шутка слишком мощная даже для Хондамира"
